0c1a06dbc59d53e7841811271c2829a0104e4dcb
matheus2ouza <matheusfurtadogg@gmail.com>
2025-07-20 22:58:44 -0300

    ajuste para recarregar a pagina

--- src/payment.js
+++ src/payment.js
@@ -525,6 +525,7 @@ async function handlePaymentSubmit(e) {
             'Pagamento Registrado',
             `Pagamento de R$ ${amount.toFixed(2).replace('.', ',')} registrado com sucesso!`
         );
+        location.reload()
     } catch (error) {
         console.error('Error de envio', error);
         showErrorMessage('Erro no envio', error.message || 'Ocorreu um erro ao enviar o arquivo')
